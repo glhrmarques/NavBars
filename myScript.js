6d0161fd58ba9df57d0a641e9b3e822fd2eba490
Guilherme <56496944+glhrmarques@users.noreply.github.com>
2021-02-22 17:55:41 -0300

    Delete myScript.js

--- myScript.js
+++
@@ -1,6 +0,0 @@
-const  toggleButton= document.getElementsByClassName('toggle-btn')[0]
-const navLinks = document.getElementsByClassName('nav-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navLinks.classList.toggle('show')
-})
