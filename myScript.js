e1ad6f46f0a78ec1761777fd69a1e00777536505
Guilherme <56496944+glhrmarques@users.noreply.github.com>
2021-02-22 17:34:28 -0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,10 +1,3 @@
-const toggleText = document.getElementsByClassName('toggle')[0]
-const showText = document.getElementsByClassName('showme')[0]
-
-toggleText.addEventListener('click', () => {
-    showText.classList.toggle('active')
-})
-
 const  toggleButton= document.getElementsByClassName('toggle-btn')[0]
 const navLinks = document.getElementsByClassName('nav-links')[0]
 
